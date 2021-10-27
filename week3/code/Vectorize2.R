ab35fdee78b086ae920f5bbdb7c50fb160735fd5
uvafyy <uvafyy@gmail.com>
2021-10-27 13:10:45 +0100

    finished vectorization2.R

--- week3/code/Vectorize2.R
+++ week3/code/Vectorize2.R
@@ -2,7 +2,7 @@
 
 rm(list = ls())
 
-stochrick <- function(p0 = runif(1000, .5, 1.5), r = 1.2, K = 1, sigma = 0.2,numyears = 100)
+stochrick <- function(p0 = runif(1000, .5, 1.5), r = 1.2, K = 1, sigma = 0.2, numyears = 100)
 {
 
   N <- matrix(NA, numyears, length(p0))  #initialize empty matrix
@@ -13,35 +13,13 @@ stochrick <- function(p0 = runif(1000, .5, 1.5), r = 1.2, K = 1, sigma = 0.2,num
 
     for (yr in 2:numyears){ #for each pop, loop through the years
 
-      N[yr, pop] <- N[yr-1, pop] * exp(r * (1 - N[yr - 1, pop] / K) + rnorm(1, 0, sigma)) # add one fluctuation from normal distribution
+      N[yr, p0] <- N[yr-1, pop] * exp(r * (1 - N[yr - 1, pop] / K) + rnorm(1, 0, sigma)) # add one fluctuation from normal distribution
     
-     }
+    }
   
   }
  return(N)
 
-}
-
-stochrickvect <- function(p0 = runif(1000, .5, 1.5), r = 1.2, K = 1, sigma = 0.2,numyears = 100)
-{
-  
-  N <- matrix(NA, numyears, length(p0))  #initialize empty matrix
-  
-  N[1, ] <- p0
-  
-  for (pop in 1:length(p0)) { #loop through the populations
-    
-    for (yr in 2:numyears){ #for each pop, loop through the years
-      
-      N[yr, pop] <- N[yr-1, pop] * exp(r * (1 - N[yr - 1, pop] / K) + rnorm(1, 0, sigma)) # add one fluctuation from normal distribution
-      
-tapply(yr in 2:numyears, function[yr, pop] N[yr-1, pop] * exp(r * (1 - N[yr - 1, pop] / K) + rnorm(1, 0, sigma))
-
-    }
-    
-  }
-  return(N)
-  
 }
 
 # Now write another function called stochrickvect that vectorizes the above to
@@ -49,3 +27,32 @@ tapply(yr in 2:numyears, function[yr, pop] N[yr-1, pop] * exp(r * (1 - N[yr - 1,
 
 # print("Vectorized Stochastic Ricker takes:")
 # print(system.time(res2<-stochrickvect()))
+
+
+stochrickvect <- function(p0 = runif(1000, .5, 1.5), r = 1.2, K = 1, sigma = 0.2, numyears = 100)
+{
+  
+  N <- matrix(NA, numyears, length(p0))  #initialize empty matrix
+  
+  N[1, ] <- p0
+  
+  for (yr in 2:numyears){ #removes the population for loop, only the year loop is kept
+    
+    N[yr, 1:length(p0)] <- N[yr-1, 1:length(p0)] * exp(r * (1 - N[yr - 1, 1:length(p0)] / K) + rnorm(1, 0, sigma)) # repeats population 1:length within the equation
+      
+  }
+    
+
+  return(N)
+  
+}
+
+
+# compares system time for both functions
+print("Stochastic Ricker takes:")
+print(system.time(res2 <- stochrick()))
+
+
+print("Vectorized Stochastic Ricker takes:")
+print(system.time(res2 <- stochrickvect()))
+
